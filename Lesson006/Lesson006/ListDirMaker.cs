7ef459c767c6063f2d98b719cd6a096ca231574d
Maxim <mbzozn@mail.ru>
2021-03-08 10:37:57 +0300

    Continue

---
+++ Lesson006/Lesson006/ListDirMaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson006
+{
+    class ListDirMaker
+    {
+        void ListDirIter(string SomePath)
+        {
+            string [] Tempo = Directory.EnumerateDirectories(SomePath);
+        }
+    }
+}
---
+++ Lesson006/Lesson006/TreeDirSavior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson006
+{
+    class TreeDirSavior
+    {
+        public static void Show()
+        {
+            Console.WriteLine("Введите корневой каталог для вывода");
+            string SomePath = Console.ReadLine();
+
+            TraverseTree(SomePath);
+        }
+    }
+}
