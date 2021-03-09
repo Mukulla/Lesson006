51f4fb29b3e0c33aa4ea9640972af69c4c6cc4b7
Maxim <mbzozn@mail.ru>
2021-03-09 15:41:47 +0300

    All Donnu

---
+++ Lesson0061/Lesson0061/MyException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson0061
+{
+    [Serializable]
+    class MyException : Exception
+    {
+        public MyArrayErrors MyErrorCode { get; } 
+        public enum MyArrayErrors
+        {
+           SizeException,
+           DataException
+        }
+        
+         public MyException(MyArrayErrors SomeCode)
+        {
+            MyErrorCode = SomeCode;
+        }
+    }
+}
---
+++ Lesson0061/Lesson0061/Shain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson0061
+{
+    class Shain
+    {
+        //ФИО, должность, email, телефон, зарплата, возраст
+        public string FIO { get; }       
+        public string Position { get; }       
+        public int Age { get; }
+
+        public string Email { get; }
+        public string Tel { get; }
+        public int Salary { get; }
+
+        public Shain()
+        {
+            FIO = "123";
+            Position = "Uto";
+            Age = 19;
+            Email = "4";
+            Tel = "567";
+            Salary = 59;
+        }
+        public Shain(string FIO001, string Position001, int Age001, string Email001, string Tel001, int Salary001)
+        {
+            FIO = FIO001;
+            Position = Position001;
+            if (Age001 < 1)
+            {
+                Age001 = 0;
+            }
+            Age = Age001;
+
+            Email = Email001;
+            Tel = Tel001;
+
+            if (Salary001 < 1)
+            {
+                Salary001 = 0;
+            }
+            Salary = Salary001;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Полное имя сотрудника: {FIO} Должность:{Position} Возраст: {Age}");
+            Console.WriteLine($"Email: {Email} Телефон: {Tel} Заработная плата: {Salary}");
+        }
+    }
+}
